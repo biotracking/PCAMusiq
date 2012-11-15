ccbc3ea259810d9e473f758f75bcf0e4b0b7638c
David Stolarsky <david.stolarsky@gmail.com>
2012-11-15 14:43:40 -0500

    usage

--- PCAMusiqPlayer/main.cpp
+++ PCAMusiqPlayer/main.cpp
@@ -1,3 +1,5 @@
+#include <string>
+
 #include <QtGui/QApplication>
 #include "MainWindow.h"
 
@@ -7,6 +9,8 @@ namespace po = boost::program_options;
 
 int main(int argc, char *argv[])
 {
+    QApplication a(argc, argv);
+
     /*po::options_description desc("Allowed options");
     desc.add_options()
         ("help", "produce help message")
@@ -25,7 +29,20 @@ int main(int argc, char *argv[])
         exit(0);
     }*/
 
-    QApplication a(argc, argv);
+    if(a.arguments().length() < 4 || a.arguments().contains("-h") || a.arguments().contains("--help"))
+    {
+        std::cout << "Usage: " << argv[0] << " <video_source> <pca> <osc_send_uri>" << std::endl;
+        std::cout << std::endl;
+        std::cout << "video_source: \"camera\" (prosilica grabber) or path to a video file" << std::endl;
+        std::cout << "pca: path to pre-computed pca (generated with e.g. pcamusiq)" << std::endl;
+        std::cout << "osc_send_uri: OSC address of MusicBox" << std::endl;
+        std::cout << std::endl;
+        std::cout << "Example: " << argv[0] << " camera ~/my_pca/ 127.0.0.1:50000" << std::endl;
+
+        exit(0);
+    }
+
+
     MainWindow w;
     w.show();
 
